c4fa58be5095ee067a9a824e9d901062aef1ed10
JorgePeju <123939712+JorgePeju@users.noreply.github.com>
2023-05-24 17:58:04 +0200

    cambiado modelo de usuario

--- models/users.js
+++ models/users.js
@@ -18,6 +18,10 @@ const UserSchema = new Schema({
         type: String,
         trim: true
     },
+    city:{
+        type: String,
+        trim: true
+    },
     uid: {
         type: String,
         
